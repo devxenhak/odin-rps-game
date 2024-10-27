bff30bee89917166f314f7472880ea2556b927ff
Mohsin <185420349+devxenhak@users.noreply.github.com>
2024-10-27 22:32:02 +0530

    create a rps game in cli

---
+++ rps.js
@@ -0,0 +1,30 @@
+let HumanScore;
+let computerScore;
+
+const getComputerChoice = () => {
+  let computerChoice;
+  const choice = Math.floor(Math.random() * 3);
+
+  if (choice === 0) {
+    computerChoice = "rock";
+  } else if (choice === 1) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissor";
+  }
+  return computerChoice;
+};
+
+console.log(getComputerChoice());
+
+const getHumanChoice = () => {
+  const humanChoice = prompt("Enter your Choice: ").toLowerCase();
+  return humanChoice;
+};
+
+const playRound = (humanChoice, computerChoice) => {};
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
